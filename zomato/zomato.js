5c4903f5886ed6acf21c00cc7632b2740a7b2ecf
MVu102 <41821657+MVu102@users.noreply.github.com>
2018-10-03 11:42:41 -0700

    Add files via upload

--- zomato/zomato.js
+++ zomato/zomato.js
@@ -1,19 +1,42 @@
 $(document).ready(function(){
 
     var APIkey = "dd597b949ad3061e6dae13b560b4afd0"
-    var queryURL ="https://developers.zomato.com/api/v2.1/search?q=Breakfast&count=5&apikey=" + APIkey
+    var queryURL ="https://developers.zomato.com/api/v2.1/search?q=&count=5&apikey=" + APIkey
     
+      //  function zomato(){
+          
         $.ajax({
             url: queryURL,
             method: "GET"
                         
             })
             .then(function (response){
-            
-                var restaurantName = response.restaurants[0];
-                console.log(restaurantName.restaurant.name)
-                $("#results").text(restaurantName.restaurant)     
+                
+                var restaurantR =response.restaurants[0].restaurant
+                var restaurantName = restaurantR.name;
+                console.log(restaurantName)
+                $("#names").text(restaurantName);
+                    $("#names").attr("href", restaurantR.url);
+                var restaurantCuisine = restaurantR.cuisines;
+                console.log(restaurantCuisine)
+                var locationR = restaurantR.location.address;
+                $("#address").text("Address: "+locationR)
+                $("#rating").text("Ratings: "+ restaurantR.user_rating.aggregate_rating)
+                 
             });
+   //     };
+        //function mealDB(){
+            $.ajax({
+                url:"https://www.themealdb.com/api/json/v1/1/list.php?a=list",
+                method: "GET"
+
+            }).then(function (response){
+                
+                var mealsArea= response.meals[0];
+                console.log(mealsArea.strArea)
+            });
+       // };
+
 
 var modal = document.getElementById("simpleModal");
 
