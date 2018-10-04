42280bf8cb705c208295be9b1f4da00bf9ab92fe
MVu102 <41821657+MVu102@users.noreply.github.com>
2018-10-03 17:06:59 -0700

    Add files via upload

--- zomato/zomato.js
+++ zomato/zomato.js
@@ -1,42 +1,47 @@
 $(document).ready(function(){
 
-    var APIkey = "dd597b949ad3061e6dae13b560b4afd0"
-    var queryURL ="https://developers.zomato.com/api/v2.1/search?q=&count=5&apikey=" + APIkey
-    
-      //  function zomato(){
-          
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-                        
-            })
-            .then(function (response){
-                
-                var restaurantR =response.restaurants[0].restaurant
-                var restaurantName = restaurantR.name;
-                console.log(restaurantName)
-                $("#names").text(restaurantName);
-                    $("#names").attr("href", restaurantR.url);
-                var restaurantCuisine = restaurantR.cuisines;
-                console.log(restaurantCuisine)
-                var locationR = restaurantR.location.address;
-                $("#address").text("Address: "+locationR)
-                $("#rating").text("Ratings: "+ restaurantR.user_rating.aggregate_rating)
-                 
-            });
-   //     };
-        //function mealDB(){
             $.ajax({
                 url:"https://www.themealdb.com/api/json/v1/1/list.php?a=list",
                 method: "GET"
 
             }).then(function (response){
                 
-                var mealsArea= response.meals[0];
+                var mealsArea= response.meals[8];
                 console.log(mealsArea.strArea)
+        
+       var APIkey = "dd597b949ad3061e6dae13b560b4afd0"
+       var queryURL ="https://developers.zomato.com/api/v2.1/search?q=" + mealsArea.strArea + "&count=5&apikey=" + APIkey
+             
+           $.ajax({
+               url: queryURL,
+               method: "GET"
+                           
+               })
+               .then(function (response){
+                   
+                for( var i =0; i<response.restaurants.length; i++){
+                   // console.log(response.restaurants[i])
+                   
+                   var responseR = response.restaurants
+                   console.log(responseR)
+                   var restaurantR =response.restaurants[i].restaurant
+                   var restaurantName = restaurantR.name;
+                   //console.log(restaurantName)
+                   var restaurantCuisine = restaurantR.cuisines;
+                  // console.log(restaurantCuisine)
+                   var locationR = restaurantR.location.address;
+                   //if (mealsArea.strArea == restaurantCuisine){
+                    responseR.forEach(function(){
+                        $("#names").text(restaurantName);
+                        //$("#names").attr("href", restaurantR.url);
+                        $("#address").text("Address: "+locationR);
+                        $("#rating").text("Ratings: "+ restaurantR.user_rating.aggregate_rating) + "<br>";
+                   // }); 
+                });
+                              
+                };
+               });
             });
-       // };
-
 
 var modal = document.getElementById("simpleModal");
 
@@ -63,5 +68,3 @@ function oustideClick(e){
     }
 }
 });
-
-
